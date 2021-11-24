cae9965368a8c162577f2774e419289d9c39be04
Adem Hatay <hatayadem5@gmail.com>
2021-11-24 16:58:27 +0300

    ex02

---
+++ c_00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/24 14:53:05 by ahatay            #+#    #+#             */
+/*   Updated: 2021/11/24 15:13:24 by ahatay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	 ft_print(char c)
+{
+	write(1,&c,1);
+}
+
+void	 ft_print_reverse_alphabet(void)
+{
+	char	 z;
+	z = 'z';
+
+	while ('a' <= z)
+	{
+		ft_print(z);
+		z--;
+	}
+}
+
+int main()
+{
+	ft_print_reverse_alphabet();
+	return 0;
+}
