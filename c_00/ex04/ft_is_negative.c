096afcd9e376c6054a4a16ad4f5187782a8182db
Adem Hatay <hatayadem5@gmail.com>
2021-11-25 16:17:37 +0300

    ex04

--- c_00/ex04/ft_is_negative.c
+++ c_00/ex04/ft_is_negative.c
@@ -6,24 +6,25 @@
 /*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/24 17:37:37 by ahatay            #+#    #+#             */
-/*   Updated: 2021/11/25 11:33:10 by ahatay           ###   ########.fr       */
+/*   Updated: 2021/11/25 13:58:39 by ahatay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-void	 ft_putchar(char c)
+
+void	ft_putchar(char c)
 {
-	write(1,&c,1);
+	write(1, &c, 1);
 }
 
-void	 ft_is_negative(int n)
+void	ft_is_negative(int n)
 {
-	char 	c;
+	char	c;
 	char	p;
-	c='N';
-	p='P';
 
-	if(n < 0)
+	c = 'N';
+	p = 'P';
+	if (n < 0)
 	{
 		ft_putchar(c);
 	}
