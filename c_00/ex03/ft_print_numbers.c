01759cc49903eac755f575ad1124b002df151d88
Adem Hatay <hatayadem5@gmail.com>
2021-11-25 16:17:31 +0300

    ex03

--- c_00/ex03/ft_print_numbers.c
+++ c_00/ex03/ft_print_numbers.c
@@ -6,23 +6,23 @@
 /*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 11:31:45 by ahatay            #+#    #+#             */
-/*   Updated: 2021/11/25 11:32:24 by ahatay           ###   ########.fr       */
+/*   Updated: 2021/11/25 13:50:48 by ahatay           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-	write(1,&c,1);
+	write(1, &c, 1);
 }
 
-void ft_print_numbers(void)
+void	ft_print_numbers(void)
 {
-	char 	i;
-	i = '0';
+	char	i;
 
-	while(i<='9')
+	i = '0';
+	while (i <= '9')
 	{
 		ft_putchar(i);
 		i++;
