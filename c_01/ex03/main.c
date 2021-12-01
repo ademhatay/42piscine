e068eaba9ee5f9f64be4375c447ec624fda85e65
Adem Hatay <hatayadem5@gmail.com>
2021-12-01 16:13:50 +0300

    ex03

---
+++ c_01/ex03/main.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/01 14:24:11 by ahatay            #+#    #+#             */
+/*   Updated: 2021/12/01 14:24:20 by ahatay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void	ft_div_mod(int a, int b, int *div, int *mod);
+int		main (void)
+{
+  int a = 5;
+  int b = 2;
+
+  int *div = &a;
+  int *mod = &b;
+  ft_div_mod(a, b, div, mod);
+
+  printf("a  : %d",*div);
+	printf("\nb : %d",*mod);
+	return (0);
+}
