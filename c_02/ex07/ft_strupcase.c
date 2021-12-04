23d6248f3829f61875f3eb030cf87aa0d5d80349
Adem Hatay <hatayadem5@gmail.com>
2021-12-05 00:42:02 +0300

    ex07

---
+++ c_02/ex07/ft_strupcase.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 16:34:15 by ahatay            #+#    #+#             */
+/*   Updated: 2021/12/04 17:51:40 by ahatay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
+
+int main()
+{
+  char dizi[] = "hepsini buyuk yap!";
+  printf("%s", ft_strupcase(dizi));
+}
