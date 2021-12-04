164cf70a86f113fd2808c4df2f5aaec04ebe6775
Adem Hatay <hatayadem5@gmail.com>
2021-12-05 00:42:06 +0300

    ex08

---
+++ c_02/ex08/ft_strlowcase.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/04 17:54:16 by ahatay            #+#    #+#             */
+/*   Updated: 2021/12/04 23:19:25 by ahatay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+		{
+			str[i] += 32;
+		}
+		i++;
+	}
+	return (str);
+}
+int main()
+{
+  char dizi[] = "HEPSINI KUCUK YAP!";
+  printf("%s", ft_strlowcase(dizi));
+}
