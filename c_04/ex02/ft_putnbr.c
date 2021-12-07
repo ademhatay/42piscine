aed0b0f862bba4a19c3547e3aa12211153028c2d
Adem Hatay <hatayadem5@gmail.com>
2021-12-07 20:01:57 +0300

    ex02

---
+++ c_04/ex02/ft_putnbr.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahatay <ahatay@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/07 17:51:16 by ahatay            #+#    #+#             */
+/*   Updated: 2021/12/07 17:51:57 by ahatay           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_putnbr(int nb)
+{
+	if (nb > -214748 || nb <= 2147483647)
+	{
+		if (nb == -2147483648)
+		{
+			ft_putchar('-');
+			ft_putchar('2');
+			ft_putnbr(147483648);
+			return ;
+		}
+		else if (nb >= 10)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else if (nb < 0)
+		{
+			nb = -nb;
+			ft_putchar('-');
+			ft_putnbr(nb);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+}
