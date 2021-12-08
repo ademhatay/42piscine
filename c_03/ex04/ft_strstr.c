b1c13a4c80a072db2d64f45da60c8205281e9b56
Adem Hatay <hatayadem5@gmail.com>
2021-12-08 18:20:40 +0300

    ex04

--- c_03/ex04/ft_strstr.c
+++ c_03/ex04/ft_strstr.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-#include <string.h>
-
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	a;
 
 	i = 0;
-	a = 0;
 	if (to_find[0] == '\0')
 		return (str);
 	while (str[i] != '\0')
 	{
-		while(str[i + a] == to_find[a] && str[i + a] != '\0')
+		a = 0;
+		while (str[i + a] == to_find[a] && str[i + a] != '\0')
 		{
 			if (to_find[a + 1] == '\0')
 				return (&str[i]);
@@ -25,10 +23,7 @@ char	*ft_strstr(char *str, char *to_find)
 
 int main()
 {
-	char dizi1[] = "Bilgisayar";
-	char dizi2[] = "isayar";
-	char *res = ft_strstr(dizi1, dizi2);
-
-	printf("%s karakter dizisinde bulunan dizi: %s, adresi: %p, indeksi:  %ld\n", dizi1, res,  res, res-dizi1);
+	char str[] = "ankara";
+	char toFind[] = "kar";
+	printf("%s", ft_strstr(str, toFind));
 }
-
